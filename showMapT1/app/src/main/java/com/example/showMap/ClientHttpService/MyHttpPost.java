1719c7951bf536cce7e2a5725f409f52517303a5
myleosu <1063371833@qq.com>
2019-03-29 11:02:37 +0800

    提交文件

--- showMapT1/app/src/main/java/com/example/showMap/ClientHttpService/MyHttpPost.java
+++ showMapT1/app/src/main/java/com/example/showMap/ClientHttpService/MyHttpPost.java
@@ -15,6 +15,10 @@ import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -36,11 +40,29 @@ public class MyHttpPost {
     private static final int SO_TIMEOUT = 0;
 
     //通过POST方式获取HTTP服务器数据
-    public static String executeHttpPost(String servlet, List<NameValuePair> params){
+    public static String executeHttpPost(String servlet,List<NameValuePair>params){
         String baseURL = SERVER + PROJECT + servlet;
+//        byte[] data = ''//获得请求体
+        URL url = null;
+
         Log.i("tag",baseURL);
         String resonseMsg = "FAILED";
         try{
+//            url = new URL(baseURL);
+//            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+//            urlConnection.setDoInput(true);     //设置输入流采用字节流
+//            urlConnection.setDoOutput(true);    //设置输出流采用字节流
+//            urlConnection.setRequestMethod("POST"); //设置请求方式为Post
+//            urlConnection.setUseCaches(false);  //设置缓存
+//            //请求的信息为文本信息
+//            urlConnection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");   //设置meta参数
+//            urlConnection.setRequestProperty("Charset","utf-8");
+//
+//            urlConnection.connect();//链接既往服务端发送消息
+//
+//            DataOutputStream dop = new DataOutputStream(urlConnection.getOutputStream());
+//            dop.write();
+
             //连接到服务器端相应的Servlet
             HttpPost request = new HttpPost(baseURL);
             request.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
@@ -61,4 +83,9 @@ public class MyHttpPost {
         Log.i("tag","responMsg = "+resonseMsg);
         return resonseMsg;
     }
+
+    /**
+     * 封装请求体信息
+     * Param
+     */
 }
